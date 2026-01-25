6f24b3e0227d34c7fee80fde4c1826dbf0f83c09
Sahil Chandel <rajputsahilchandel@gmail.com>
2026-01-25 16:25:29 +0530

    fix: added schema type

--- src/common/zod-validation.pipe.ts
+++ src/common/zod-validation.pipe.ts
@@ -1,9 +1,9 @@
 import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
-import { ZodSchema, ZodError } from 'zod';
+import { ZodType, ZodError } from 'zod';
 
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
-  constructor(private schema: ZodSchema) {}
+  constructor(private schema: ZodType) {}
 
   transform(value: unknown) {
     try {
